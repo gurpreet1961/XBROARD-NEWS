97593b04afc08ec5096f6e95ae2e0c7b105cf0c8
Crio.GitLab <admin@example.com>
2022-04-19 08:40:39 +0000

    Update resources/data/magazines.js

--- resources/data/magazines.js
+++ resources/data/magazines.js
@@ -7,4 +7,10 @@ magazines = [
 /** Caution: Do not write your logic in this file.
  * This file gets replaced during assessment with the original content that
  * we have for assessment. If you write your code in this file, you will end up deleting it in the assessment.
+ * Uncomment the below lines to do local assessment
  */
+// magazines = [
+//   "https://assessment-rss.s3.ap-south-1.amazonaws.com/coronavirus.rss",
+//   "https://assessment-rss.s3.ap-south-1.amazonaws.com/india-tech.rss",
+//   "https://assessment-rss.s3.ap-south-1.amazonaws.com/sports-star.rss",
+// ];
